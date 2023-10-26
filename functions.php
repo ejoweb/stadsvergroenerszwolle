e94ef93e90e97e54c1e9110459216d149cdab58a
Erik Joling <info@ejoweb.nl>
2023-10-26 10:37:10 +0200

    Remove unused function for custom block class

--- functions.php
+++ functions.php
@@ -24,9 +24,6 @@ add_action( 'after_setup_theme', function() {
 	// Enqueue scripts and styles to the editor
 	add_action( 'enqueue_block_editor_assets', 'svz_add_editor_styles_and_scripts' );
 
-	// Add block styles (classes)
-	// add_action( 'init', 'svz_register_block_styles' );
-
 	// Disable styles of plugin Lightweight accordion	 
 	add_filter('lightweight_accordion_include_frontend_stylesheet', '__return_false' );
 	add_filter('lightweight_accordion_include_admin_stylesheet', '__return_false' );
@@ -89,19 +86,4 @@ function svz_add_editor_styles_and_scripts() {
 
 	// Enqueue theme stylesheet.
 	wp_enqueue_style( 'svz-style-for-editor' );
-}
-
-/**
- * Add block styles (classes)
- */
-function svz_register_block_styles() {
-
-	// Special group block
-    register_block_style(
-        'core/button', // name of your block
-        array(
-            'name'  => 'ghub-button-1', // part of the class that gets added to the block.
-            'label' => __( ' Ghub Style 1', 'style-1' ),
-        )
-    );
 }
