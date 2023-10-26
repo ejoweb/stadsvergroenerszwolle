6935d02b07d6f172017fa60ba37967c0759a3a29
Erik Joling <info@ejoweb.nl>
2023-10-26 10:37:28 +0200

    Remove block styles support to fix image spacing

--- functions.php
+++ functions.php
@@ -12,9 +12,6 @@
  */
 add_action( 'after_setup_theme', function() {
 
-	// Add support for block styles.
-	add_theme_support( 'wp-block-styles' );
-
 	// Enqueue editor styles.
 	add_editor_style( 'style.css' );
 
